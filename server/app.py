261ea1b75f902077b14fe252ffab2f051d550c6f
Sanaya James <sanaya.j.james@gmail.com>
2026-08-25 13:04:55 -0700

    routes for link resources

--- server/app.py
+++ server/app.py
@@ -11,6 +11,8 @@ from controllers.ArtworkReviewIndex import ArtworkReviewIndex
 from controllers.ArtworkReviewById import ArtworkReviewById
 from controllers.ArtistReviewIndex import ArtistReviewIndex
 from controllers.ArtistReviewById import ArtistReviewById
+from controllers.ArtworkCollectionLink import ArtworkCollectionLink
+from controllers.ArtistCollectionLink import ArtistCollectionLink
 
 # landing page/home page
 # alternatively, landing page is user's own collections to avoid a typical social media setup
@@ -29,3 +31,6 @@ api.add_resource(ArtworkReviewById, '/artwork-reviews/<int:artwork_review_id>')
 
 api.add_resource(ArtistReviewIndex, '/artist-reviews')
 api.add_resource(ArtistReviewById, '/artist-reviews/<int:artist_review_id>')
+
+api.add_resource(ArtworkCollectionLink, '/collections/<int:collection_id>/artwork-reviews/<int:artwork_review_id>')
+api.add_resource(ArtistCollectionLink, '/collections/<int:collection_id>/artist-reviews/<int:artist_review_id>')
