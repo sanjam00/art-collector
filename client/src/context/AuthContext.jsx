535f94f349ac9b81f9c8c3c099e114a08d0c98a6
Sanaya James <sanaya.j.james@gmail.com>
2026-08-28 11:54:38 -0700

    implemented api.js and AuthContext.js

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+// authorization (where the token actually lives)
+
+import { createContext, useContext, useState } from "react";
+import { apiFetch } from "../api/api";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({children}) {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+
+  async function login(username, password) {
+    const data = await apiFetch('/login', null, {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+    });
+    setToken(data.token);
+    setUser(data.user);
+  }
+
+  async function signup(username, email, password, passwordConfirmation) {
+    const data = await apiFetch('/signup', null, {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    });
+    setToken(data.token);
+    setUser(data.user);
+  }
+
+  function logout() {
+    setToken(null);
+    setUser(null);
+  }
+
+  return(
+    <AuthContext.Provider value={{ token, user, login, signup, logout }} >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// this is what gets called in other functions to actually use the AuthContext
+export function useAuth() {
+  return useContext(AuthContext)
+}
