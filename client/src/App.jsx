2bb5982fed5d44e10bd8c120c1f47c299c3ba21e
Sanaya James <sanaya.j.james@gmail.com>
2026-08-31 12:37:40 -0700

    not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <Routes>
+          < Route path="/*" element= { < NotFoundPage /> } />
           < Route path="/login" element={ <LoginPage /> } />
         </Routes>
       </BrowserRouter>
