b752c8e75fe2e9f4b621598d0d3eeef22990eece
Sanaya James <sanaya.j.james@gmail.com>
2026-09-02 18:12:48 -0700

    styled homefeed in new css file. added variables for box color and shadow

--- client/src/pages/HomeFeed.jsx
+++ client/src/pages/HomeFeed.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { getCollectionFeed } from "../services/CollectionService";
 import { useNavigate } from "react-router";
+import "../styles/HomeFeed.css"
 
 export default function HomeFeed(){
   const [collections, setCollections] = useState([]);
@@ -32,9 +33,11 @@ export default function HomeFeed(){
 
       <div className="collection-grid">
         {collections.map((c) => (
-        <div className="collec-card" onClick={() => handleNavigate(c)}>
-          <img src={c.collection_img} />
-          <p key={c.id}>{c.title}</p>
+        <div className="collection-item" onClick={() => handleNavigate(c)}>
+          <div className="card">
+            <img className="card-img-top" src={c.collection_img} alt={c.title + "Image"} />
+            <p key={c.id} className="card-body">{c.title}</p>
+          </div>
         </div>
       ))}
       </div>
