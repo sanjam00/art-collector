e0657bd42b6336a55cb8cf3d31522c3ea8740c14
Sanaya James <sanaya.j.james@gmail.com>
2026-09-02 21:31:26 -0700

    more icons

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -2,26 +2,33 @@ import { NavLink } from "react-router";
 import "../../styles/Navbar.css";
 import homeIcon from "../../styles/icons/house-door.svg";
 import homeFillIcon from "../../styles/icons/house-door-fill.svg";
+// import profileCircleIcon from "../../styles/icons/person-circle.svg"
+import profileIcon from "../../styles/icons/person.svg"
+import profileFillIcon from "../../styles/icons/person-fill.svg"
+import plusCircleIcon from "../../styles/icons/plus-circle.svg"
+import plusCircleFillIcon from "../../styles/icons/plus-circle-fill.svg"
 
 export default function NavBar() {
-
 
   return (
     <div className="navbar-expand{-lg}">
       <nav className="navbar">
 
       <div className="nav-icons">
-        <NavLink to='/trips/new'> 
-          ➕ {/* needs to trigger a pop up that will take user to appropriate adding page */}
+          {/* needs to trigger a pop up that will take user to appropriate adding page */}
+        <NavLink to='/trips/new' className="icon-hover"> 
+          <img className="bi bi-plus-circle icon-default" src={plusCircleIcon} />
+          <img className="bi bi-plus-circle icon-hover-state" src={plusCircleFillIcon} />
         </NavLink>
 
         <NavLink to='/home' className="icon-hover">
           <img className="bi bi-house-door icon-default" src={homeIcon} />
-          <img className="bi bi-house-door icon-hover-state" src={homeFillIcon} />
+          <img className="bi bi-house-door-fill icon-hover-state" src={homeFillIcon} />
         </NavLink>
 
-        <NavLink to='/whoami'>
-          🚹 {/* not setup yet */}
+        <NavLink to='/whoami' className="icon-hover"> {/* not setup yet */}
+          <img className="bi bi-person-circle icon-default" src={profileIcon} /> 
+          <img className="bi bi-person-fill icon-hover-state" src={profileFillIcon} />
         </NavLink>
       </div>
 
