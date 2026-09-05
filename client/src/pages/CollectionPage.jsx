480fe416d1db7566a53a30e2794347cc68c8cccc
Sanaya James <sanaya.j.james@gmail.com>
2026-09-05 16:37:13 -0700

    added more data to collectionpage.jsx

--- client/src/pages/CollectionPage.jsx
+++ client/src/pages/CollectionPage.jsx
@@ -3,6 +3,10 @@ import { useAuth } from "../context/AuthContext";
 import { getCollectionById } from "../services/CollectionService";
 import { useParams } from "react-router";
 
+import coverArtPlaceholder from "../styles/icons/cover-art-placeholder.svg"
+import lockIcon from "../styles/icons/lock.svg"
+import globeIcon from "../styles/icons/globe2.svg"
+
 export default function CollectionPage(){
   const [collectionData, setCollectionData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -30,7 +34,23 @@ export default function CollectionPage(){
 
   return (
     <div>
-      <h1>{collectionData.title}</h1>
+      <div className="collection-header">
+        {/* need to render the first image from a review if available */}
+        <img src={collectionData.collection_img || coverArtPlaceholder} />
+        <h1>{collectionData.title}</h1>
+        <p>{collectionData.description}</p>
+        <p>{collectionData.username}</p>
+        {/* conditionally render a lock = private || globe = public */}
+        <img 
+          className={collectionData.is_public ? "bi bi-globe2" : "bi bi-lock"} 
+          src={collectionData.is_public ? globeIcon : lockIcon} 
+        />
+
+      </div>
+
+      <div className="collection-items">
+        {/* map through reviews, style similarly to homefeed collections */}
+      </div>
     </div>
   )
 }
