06a26e748c2d240acc42d357e51f28f59c893dee
Sanaya James <sanaya.j.james@gmail.com>
2026-09-11 14:33:03 -0700

    added user_id to schema, required for patch and delete auth

--- server/models/schemas/ArtistReviewSchema.py
+++ server/models/schemas/ArtistReviewSchema.py
@@ -8,3 +8,4 @@ class ArtistReviewSchema(Schema):
   item_img = fields.Url()
   reason_for_liking = fields.Str()
   location_viewed = fields.Str()
+  user_id = fields.Int(dump_only=True) # required for PATCH and DELETE auth
--- server/models/schemas/ArtworkReviewSchema.py
+++ server/models/schemas/ArtworkReviewSchema.py
@@ -9,3 +9,4 @@ class ArtworkReviewSchema(Schema):
   item_img = fields.Url()
   reason_for_liking = fields.Str()
   location_viewed = fields.Str()
+  user_id = fields.Int(dump_only=True) # required for PATCH and DELETE auth
